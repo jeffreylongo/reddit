4fa57df79cfe3e083d2a841fde2acc0f02a3ea88
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-22 11:21:57 -0400

    program files.

---
+++ reddit/reddit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="reddit.MvcApplication" Language="C#" %>
---
+++ reddit/reddit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(reddit.Startup))]
+namespace reddit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
